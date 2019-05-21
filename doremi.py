248b39c62712cde0d3e518c69be0a67ebd3c7cd2
Prunoideae <a455167189@163.com>
2019-05-21 23:14:33 +0800

    Revert "test fix"
    This reverts commit 5eb51004581d303c3374a4e45da27cbdcee756d6.

--- doremi.py
+++ doremi.py
@@ -5,8 +5,7 @@ import validators
 import sys
 import requests
 import os
-import importlib.machinery
-import types
+import importlib
 
 TOKEN = 'NTgwMjU2MTgwNzk5MTQzOTM2.XOOEaA.2wG1z7hrPFyfk8W-MzJ3JUd7qW0'
 
@@ -42,11 +41,11 @@ async def on_message(message):
         script_name, url = message.content.replace("!import ", "").split(" ")
         if validators.url(url):
             r = requests.get(url)
-            with open("\scripts\\" + script_name + ".py", "w") as f:
+            with open(pypath + "\scripts\\" + script_name + ".py", "w") as f:
                 f.write(r.text)
                 f.close()
             await client.send_message(message.channel, "{0.author.mention} Script imported.".format(message))
-            await client.send_message(message.channel, os.path.abspath("\scripts\\" + script_name + ".py"))
+            await client.send_message(message.channel, os.path.abspath(pypath + "\scripts\\" + script_name + ".py"))
         else:
             msg = '{0.author.mention} URL provided is not valid!'.format(message)
             await client.send_message(message.channel, msg)
@@ -54,14 +53,11 @@ async def on_message(message):
 
     if message.content.startswith("!"):
         command_name = message.content.replace("!", "", 1).split(" ")[0]
-        if os.path.isfile('\scripts\\' + command_name + ".py"):
-            loader = importlib.machinery.SourceFileLoader(command_name, '\scripts\\' + command_name + ".py")
-            script = types.ModuleType(loader.name)
-            loader.exec_module(script)
+        if os.path.isfile(pypath + '\scripts\\' + command_name + ".py"):
+            script = importlib.import_module("scripts."+command_name)
             await script.message(client, message)
         else:
             await client.send_message(message.channel, "Script not found!")
-
 
 @client.event
 async def on_ready():
@@ -71,5 +67,4 @@ async def on_ready():
     print('------')
 
 
-os.mkdir("\scripts\\")
 client.run(TOKEN)
